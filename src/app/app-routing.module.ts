41e1a1d4f5a19b2966407c5ce6fddc68f0e51341
Toky RANDRIAMBOLA <toky.randriambola@netapsys.fr>
2018-02-27 17:42:56 +0300

    modification erreur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ListSurveysComponent} from './list-surveys/list-surveys.component';
 import {DetailSurveyComponent} from './detail-survey/detail-survey.component';
-import {homedir} from "os";
-import {HomeComponent} from "./home/home.component";
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
